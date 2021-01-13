e0d6f6b4489f39d44d593c3cd85aba770f56fa85
Kwon Eun Jee <passby_time@naver.com>
2021-01-13 23:34:58 +0900

    feat. 객체 생성 및 JSX 작성

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,44 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [products] = useState([
+    {
+      name: 'Magic Keyboard',
+      color: '실버',
+      cost: '12.99',
+      image: '/static/media/MQ052KH.5d2a82a4.jpg',
+    },
+    {
+      name: 'Magic Keyboard',
+      color: '스페이스 그레이',
+      cost: '13.99',
+      image: '/static/media/MRMH2KH.65d0cdfc.jpg',
+    },
+    {
+      name: 'Magic Trackpad',
+      color: '실버',
+      cost: '2.99',
+      image: '/static/media/MJ2R2.a2ca8f6d.jpg',
+    },
+    {
+      name: 'Magic Trackpad',
+      color: '스페이스 그레이',
+      cost: '3.99',
+      image: '/static/media/MRMF2.459ea8b1.jpg',
+    },
+  ])
   return (
     <div className="App">
+      <h1>Products</h1>
+      {products.map((product) => (
+        <div>
+          <h3>{product.name} </h3>
+          <h4>$ {product.cost}</h4>
+          <img src={product.image} alt={product.name} />
+          <button>Add to Cart</button>  
+        </div>
+      ))}
     </div>
   );
 }
